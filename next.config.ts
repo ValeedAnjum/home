b56e3bf67f9ea1b4c1e2050c4949d0578ab91e50
Waleed Anjum <allinone644@gmail.com>
2025-01-11 22:20:06 +0500

    test

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/port", // Replace with your repository name
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
