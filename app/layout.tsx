1e76d8003246ec21142dea3599733660bf9c7210
Waleed Anjum <allinone644@gmail.com>
2025-01-11 21:47:51 +0500

    BareMinimum

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,7 @@
 
 import "./globals.css";
 import { ReactNode } from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { DM_Sans } from "next/font/google";
-
-import theme from "../theme";
-
-import { AdminDashboard } from "@/layouts";
 
 const dm_sans = DM_Sans({
   subsets: ["latin"],
@@ -18,12 +12,7 @@ const dm_sans = DM_Sans({
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={dm_sans.className}>
-      <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <AdminDashboard>{children}</AdminDashboard>
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  output: "export",
-  basePath: "/",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
